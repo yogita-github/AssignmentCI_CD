6393ce8993a0337bc6c8702ed9ddaf343ea58d69
Mahajan Yogita Vinayak <103029645+yogita-github@users.noreply.github.com>
2025-05-12 15:56:02 +0530

    Update send-email.bat

--- scripts/send-email.bat
+++ scripts/send-email.bat
@@ -6,10 +6,10 @@ echo ===============================
 REM Change this section based on your SMTP setup
 set SUBJECT="Build Completed"
 set BODY="Build completed successfully. Please find attached build log."
-set TO="mentor@example.com"
+set TO="yvmahajan20@gmail.com"
 set ATTACHMENT=build.log
 set SMTP_SERVER="smtp.yourserver.com"
-set FROM="ci@example.com"
+set FROM="yogitavm2001@gmail.com"
 
 powershell -Command ^
 Send-MailMessage -To "%TO%" -From "%FROM%" -Subject %SUBJECT% -Body %BODY% -SmtpServer %SMTP_SERVER% -Attachments "%ATTACHMENT%" -UseSsl
